d58f96695902c9bce47959cbcaa5a6f6333af2d9
Marina Snitko <53336362+Lukovkammm@users.noreply.github.com>
2021-01-19 14:45:48 +0300

    feat: add script for selecting the color and size of brush

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const color = document.querySelector('input[type=color]');
+const lineWidth = document.querySelector('input[type=range]');
 
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
-ctx.lineWidth = 10;
-let maxWidth = 100;
-let colorValue = 255;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-
-let red = 0;
 
 let isDrawing = false;
 
 function draw(e) {
     if (!isDrawing) return;
-    ctx.strokeStyle = `rgba(${red}, 200, 60, .5)`;
+    
+    ctx.strokeStyle = color.value;
+    ctx.lineWidth = lineWidth.value;
 
     let lineX = e.clientX;
     let lineY = e.clientY;
@@ -23,31 +22,6 @@ function draw(e) {
     ctx.moveTo(lineX, lineY);
     ctx.lineTo(lineX, lineY);
     ctx.stroke();
-
-    if (red < colorValue) {
-        red++;
-        if (red == 255) {
-            colorValue = 1;
-        }
-    } else {
-        red--;
-        if (red == 0) {
-            colorValue = 255;
-        }
-    }
-
-    if (ctx.lineWidth >= maxWidth) {
-        maxWidth = 10;
-        ctx.lineWidth--;
-        if (ctx.lineWidth == 10) {
-            maxWidth = 100;
-        }
-    } else {
-        ctx.lineWidth++;
-    }
-
-
-
 }
 
 canvas.addEventListener('mousemove', draw);
